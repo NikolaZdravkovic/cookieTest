702a48b74b00a78f1b12dc74e11e5ce20094b1df
Nikola Zdravkovic <zdravko.dzoni@gmail.com>
2022-07-01 13:22:49 +0200

    update cookie

--- js.js
+++ js.js
@@ -1,38 +1,76 @@
 
-        var midnight = new Date();
-        midnight.setHours(24, 0, 0, 0);
-        var now = new Date();
-        var msToMidnight = midnight - now;
-        var cookieExists = document.cookie.match(/^(.*;)?\s*expireCookie\s*=\s*[^;]+(.*)?$/);
+var midnight = new Date();
+midnight.setHours(24, 0, 0, 0);
+var now = new Date();
+var msToMidnight = midnight - now;
+var cookieExists = document.cookie.match(/^(.*;)?\s*expireCookie\s*=\s*[^;]+(.*)?$/);
+var cookieName = document.cookie;
+var cookieString = cookieName.substring(cookieName.indexOf('=') + 1);;
 
-        function createCookie(name, value, minutes) {
-            if (minutes) {
-                var date = new Date();
-                date.setTime(date.getTime() + (minutes * 60 * 1000));
-                var expires = "; expires=" + date.toGMTString();
-            } else {
-                var expires = "";
-            }
-            document.cookie = name + "=" + value + expires + "; path=/";
-        }
 
-        function checkCookie() {
-            if (!cookieExists) {
-                console.log('ne postoji');
-                createCookie("expireCookie", "value", 1);
-            }
-        }
+var randomNum;
+var cookie = true;
 
-        function setCookieMidnight() {
-            if (cookieExists) {
-                createCookie("expireCookie", "value", 1);
-            }
-        }
-        var myTimeout = setTimeout(setCookieMidnight, msToMidnight);
+console.log(cookieString)
 
-        // Click event trigger new cookie
-        document.addEventListener('click', function (e) {
-            createCookie("expireCookie", "value", 1);
-        });
+function createCookie(name, value, minutes) {
+    if (minutes) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+function greet() {
+    cookie = false;
+}
+function setStorageExpire() {
+    sessionStorage.setItem('key', randomNum);
+    let data = sessionStorage.getItem('key');
+    console.log(data);
+}
 
-        checkCookie();
+function checkCookie() {
+
+    if (!cookieExists) {
+        randomNum = Math.floor(Math.random() * 100000) + 1;
+        console.log('ne postoji');
+        setStorageExpire();
+        createCookie("expireCookie", randomNum, 0.5);
+    }
+}
+
+function setCookieMidnight() {
+    if (cookieExists) {
+        createCookie("expireCookie", randomNum, 0.5);
+    }
+}
+var timeOutMid = setTimeout(setCookieMidnight, msToMidnight);
+
+// Click event trigger new cookie
+document.addEventListener('click', function (e) {
+
+    if (sessionStorage.getItem('key') === null) {
+        console.log('problem')
+        randomNum = Math.floor(Math.random() * 100000) + 1;
+        setStorageExpire();
+        createCookie("expireCookie", randomNum, 0.5);
+    }
+ 
+    if (cookie === false) {
+        cookie = true;
+        randomNum = Math.floor(Math.random() * 100000) + 1;
+        setStorageExpire();
+        createCookie("expireCookie", randomNum, 0.5);
+
+    } else {
+        console.log(cookie);
+        setTimeout(greet, 30000);
+        // randomNum = sessionStorage.getItem('key');
+        createCookie("expireCookie", randomNum, 0.5);
+    }
+
+});
+checkCookie();
